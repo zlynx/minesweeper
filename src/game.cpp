77541ecedf5e8ddffa401766fa21a1c9a546abc4
Jonathan Briggs <zlynx@acm.org>
2018-09-13 01:53:05 -0600

    Complete the game.

--- src/game.cpp
+++ src/game.cpp
@@ -1,7 +1,10 @@
+#include <cctype>
 #include <iomanip>
 #include <iostream>
 #include <random>
+#include <sstream>
 #include <string>
+#include <tuple>
 #include <vector>
 #include "game"
 
@@ -19,7 +22,8 @@ namespace zlynx { namespace minesweeper {
 
 			CLEAR = ' ',
 			MINE = 'X',
-			UNKNOWN = '?',
+			EXPLOSION = '*',
+			UNKNOWN = '.',
 			HIDDEN_CLEAR = CLEAR|HIDDEN_BIT,
 			HIDDEN_MINE = MINE|HIDDEN_BIT,
 		};
@@ -49,7 +53,55 @@ namespace zlynx { namespace minesweeper {
 			return (val & HIDDEN_MASK) == MINE;
 		}
 
+		size_t count_mines(size_t x, size_t y) {
+			size_t r = 0;
+			if(x<1 || x>width()) return r;
+			if(y<1 || y>height()) return r;
+
+			r += is_mine(get_hidden(x-1, y-1));
+			r += is_mine(get_hidden(x+0, y-1));
+			r += is_mine(get_hidden(x+1, y-1));
+
+			r += is_mine(get_hidden(x-1, y+0));
+			r += is_mine(get_hidden(x+1, y+0));
+
+			r += is_mine(get_hidden(x-1, y+1));
+			r += is_mine(get_hidden(x+0, y+1));
+			r += is_mine(get_hidden(x+1, y+1));
+
+			return r;
+		}
+
 		void reveal(size_t x, size_t y) {
+			if(x<1 || x>width()) return;
+			if(y<1 || y>height()) return;
+
+			uint8_t& item = m_items.at( vector_position(x, y) );
+			if( (item & HIDDEN_BIT) == 0) return;
+			item &= HIDDEN_MASK;
+			size_t count = count_mines(x,y);
+			if(item == CLEAR) {
+				if(count>0) {
+					item = '0'+count;
+				} else {
+					// reveal more
+					reveal(x-1, y-1);
+					reveal(x+0, y-1);
+					reveal(x+1, y-1);
+
+					reveal(x-1, y+0);
+					reveal(x+1, y+0);
+
+					reveal(x-1, y+1);
+					reveal(x+0, y+1);
+					reveal(x+1, y+1);
+				}
+			}
+		}
+
+		void reveal_all() {
+			for(auto& c: m_items)
+				c &= HIDDEN_MASK;
 		}
 	private:
 		size_t m_width, m_height;
@@ -57,12 +109,12 @@ namespace zlynx { namespace minesweeper {
 		vector<uint8_t> m_items;
 
 		size_t vector_position(size_t x, size_t y) const {
-			return (y+1)*(m_width+2) + (x+1);
+			return (y)*(m_width+2) + (x);
 		}
 
 		void distribute_mines() {
-			uniform_int_distribution<size_t> dis_x(0, m_width);
-			uniform_int_distribution<size_t> dis_y(0, m_height);
+			uniform_int_distribution<size_t> dis_x(1, m_width);
+			uniform_int_distribution<size_t> dis_y(1, m_height);
 			for(size_t i=0; i<m_mines; i++) {
 				// Pick a random location.
 				size_t x = dis_x(gen);
@@ -92,7 +144,6 @@ namespace zlynx { namespace minesweeper {
 	void Game::tick() {
 		render();
 		inputs();
-		m_running = false;
 	}
 
 	void Game::render() {
@@ -111,9 +162,9 @@ namespace zlynx { namespace minesweeper {
 		cout << '+' << '\n';
 
 		// Board contents
-		for(size_t y=0; y<m_board->height(); y++) {
+		for(size_t y=1; y<=m_board->height(); y++) {
 			cout << setw(indent_width) << y << '|';
-			for(size_t x=0; x<m_board->width(); x++) {
+			for(size_t x=1; x<=m_board->width(); x++) {
 				cout << m_board->get(x, y);
 			}
 			cout << '|' << '\n';
@@ -131,12 +182,48 @@ namespace zlynx { namespace minesweeper {
 		cout << ' ' << '\n';
 	}
 
+	std::tuple<size_t, size_t, bool> Game::parse_location(const std::string& line) {
+		size_t x, y;
+		if(line.length() < 2)
+			return make_tuple(0, 0, false);
+		istringstream is(line);
+		int xchar = is.get();
+		x = toupper(xchar) - 'A' + 1;
+		is >> y;
+		if(x > m_board->width() || y > m_board->height())
+			return make_tuple(0, 0, false);
+		if(!is)
+			return make_tuple(0, 0, false);
+		return make_tuple(x, y, true);
+	}
+
 	void Game::inputs() {
-		cout << "Enter location (eg A1 or B9):";
 		string input;
-		if( getline(cin, input) ) {
-		} else {
-			m_running = false;
-		}
+		do {
+			cout << "Enter location (eg A1 or B9): ";
+			if( getline(cin, input) ) {
+				size_t x, y;
+				bool ok;
+				tie(x, y, ok) = parse_location(input);
+				if(!ok)
+					continue;
+				m_board->reveal(x,y);
+				if(m_board->is_mine(m_board->get(x,y))) {
+					m_board->set(x,y,Board::EXPLOSION);
+					m_board->reveal_all();
+					m_running = false;
+					render();
+				}
+				break;
+			} else {
+				if(cin.eof()) {
+					m_running = false;
+					break;
+				}
+				cin.clear();
+				cin.ignore(4000,'\n');
+				continue;
+			}
+		} while(true);
 	}
 }}
